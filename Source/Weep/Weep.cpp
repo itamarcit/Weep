0457d700b4d237fa51bd875c61495d6ca0ab53a5
Itamar <itacitrin@gmail.com>
2026-06-08 18:41:31 +0300

    first commit

---
+++ Source/Weep/Weep.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Weep.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Weep, "Weep" );
+
+DEFINE_LOG_CATEGORY(LogWeep)
