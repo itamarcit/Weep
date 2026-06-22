de2d347a22410a629884a9a07caa8df106a2774a
elibrown <elizvibrown@gmail.com>
2026-06-22 18:45:56 +0300

    Starting Project File

--- Source/Weep/Weep.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Weep.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Weep, "Weep" );
-
-DEFINE_LOG_CATEGORY(LogWeep)
--- Source/Weep/WeepGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WeepGameMode.h"
-
-AWeepGameMode::AWeepGameMode()
-{
-	// stub
-}
